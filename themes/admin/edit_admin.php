65a96d695f9f424d7506990c2ef7b4b876519443
hobrt <contact@hobrt.me>
2019-07-19 04:31:36 +0100

    new update

--- themes/admin/edit_admin.php
+++ themes/admin/edit_admin.php
@@ -47,20 +47,6 @@
 		      </div>
 		    </div>
 
-		    <?php if(get_info("admins", is_login("admin_login"), "cas") != 2){ ?>
-
-		    <div class="form-group" data-sr="wait 0.6s, then enter bottom and hustle 100%">
-		      <label for="inputUser" class="col-lg-2 control-label">الصلاحيات </label>
-		      <div class="col-lg-10 input-grup">
-		      	<select class="form-control" name="cas">
-		      		<option value="1">مدير</option>
-		      		<option value="2" <?php if($key->cas == 2) echo "selected"; ?>>مشرف</option>
-		      	</select>
-		      </div>
-		    </div>
-
-			<?php } ?>
-
 		    <div class="form-group" data-sr="wait 0.3s, then enter bottom and hustle 100%">
 		      <div class="col-xs-10 col-xs-pull-2">
 		        <button type="reset" class="btn btn-default">أبدء من جديد ؟</button>
