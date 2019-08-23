0d495f48cb2013470339d28ec66eade317efc76d
hobrt <contact@hobrt.me>
2019-08-23 04:18:01 +0100

    zoom fix

--- themes/hobrtEcom/footer.php
+++ themes/hobrtEcom/footer.php
@@ -11,6 +11,21 @@
 				</div>
 			</div>
 		</div>
+
+		<div class="lx-popup">
+			<div class="lx-popup-inside">
+				<a href="javascript:;"><i class="material-icons lx-remove">close</i></a>
+				<a href="javascript:;"><i class="material-icons lx-angle-left">keyboard_arrow_left</i></a>
+				<a href="javascript:;"><i class="material-icons lx-angle-right">keyboard_arrow_right</i></a>
+				<div class="lx-popup-content">
+					<div class="lx-popup-image">
+						<img src="#" alt="image title here" />
+					</div>
+				</div>
+			</div>
+		</div>
+
+		
 		<!-- JQuery -->
 		<script src="assets/js/jquery-1.12.4.min.js"></script>
 		<!-- Swipe Script -->
